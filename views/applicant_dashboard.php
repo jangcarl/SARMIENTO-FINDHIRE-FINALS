e1b51544f1af8ea18243dc0c957921cbbf22670d
jangcarl <70869323+jangcarl@users.noreply.github.com>
2024-12-13 18:43:57 +0800

    Update applicant_dashboard.php

--- views/applicant_dashboard.php
+++ views/applicant_dashboard.php
@@ -17,7 +17,7 @@ $jobPosts = fetchJobPosts();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../styles/application_dashboard.css">
+    <link rel="stylesheet" href="../styles/applicant_dashboard.css">
     <title>Applicant Dashboard - FindHire</title>
 </head>
 <body>
