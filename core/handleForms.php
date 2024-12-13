f7d23c27bf858591348cc674a0c1d7a3784f23ea
jangcarl <70869323+jangcarl@users.noreply.github.com>
2024-12-13 18:27:50 +0800

    Update handleForms.php

--- core/handleForms.php
+++ core/handleForms.php
@@ -1,6 +1,6 @@
 <?php
-require_once 'dbConfig.php';
-require_once 'models.php';
+require_once '../dbConfig.php';
+require_once '../models.php';
 session_start(); // Ensure session is started
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
