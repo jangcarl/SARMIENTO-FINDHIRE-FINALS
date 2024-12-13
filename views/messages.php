4aff957b8728b52b8357add2e7af018d02e6d7f7
jangcarl <70869323+jangcarl@users.noreply.github.com>
2024-12-13 18:27:18 +0800

    Update messages.php

--- views/messages.php
+++ views/messages.php
@@ -1,6 +1,6 @@
 <?php
-require_once 'core/dbConfig.php';
-require_once 'core/models.php';
+require_once '../core/dbConfig.php';
+require_once '../core/models.php';
 
 if (!isset($_SESSION['user'])) {
     header('Location: login.php');
